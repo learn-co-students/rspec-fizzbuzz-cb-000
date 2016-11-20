b86d57b2bbda1418ef0fca0e67e297930fd65244
Marcus Magnusson <marcusmagnusson@outlook.com>
2016-11-20 20:35:34 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,13 @@
-
-
 def fizzbuzz(n)
   by3 = (n % 3 == 0)
   by5 = (n % 5 == 0)
   case
       when by3 && by5
           then "FizzBuzz"
+        when by5
+            then "Buzz"
       when by3
           then "Fizz"
-      when by5
-          then "Buzz"
-
   else
   end
 end
