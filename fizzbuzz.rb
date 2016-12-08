a1a067d85b37315eda3ff1c8a173e9ac2b78a6bf
Ed Dorno <edjdorno@yahoo.com>
2016-12-08 16:54:22 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,11 @@
+def fizzbuzz(num)
+  if ((num % 3 == 0) && (num % 5 == 0))
+    fizz_15 = "FizzBuzz"
+  elsif (num % 3) == 0
+    fizz_3 = "Fizz"
+  elsif (num % 5) == 0
+    fizz_5 = "Buzz"
+  else
+    nil
+  end
+end
