bba41ae0d39f4cb322ba4d63d8ab6c75a31f2f1e
Agustin Botella <agustinbotella90@gmail.com>
2016-12-24 17:15:41 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+def fizzbuzz(number)
+
+  if number % 15 == 0
+    return "FizzBuzz"
+  end
+
+  if number % 3 == 0
+    return "Fizz"
+  end
+
+  if number % 5 == 0
+    return "Buzz"
+  end
+
+end
