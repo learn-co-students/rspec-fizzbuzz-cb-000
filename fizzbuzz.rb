b6087b8ce8135fb87be0050af013a9072f1f5525
Alex Segers <a.segers.97@gmail.com>
2017-02-23 20:03:53 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
-def fizzbuzz(x)
-  if x % 3 == 0 && x % 5 == 0
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
     return "FizzBuzz"
-  elsif x % 3 == 0
+  elsif int % 3 == 0
     return "Fizz"
-  elsif x % 5 == 0
+  elsif int % 5 == 0
     return "Buzz"
   end
 end
