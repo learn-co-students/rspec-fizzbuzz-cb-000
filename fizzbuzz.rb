c7c3b32575b2ec6d2dfcf6c4611ab42bb7d67374
Michael Palmer <lawlux@gmail.com>
2017-07-05 17:27:32 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,19 @@
+def fizzbuzz(int)
+
+  if int % 3 == 0 && int % 5 == 0
+
+    #fizz_15 = fizzbuzz(15)
+    "FizzBuzz"
+
+    elsif int % 3 == 0
+    #fizz_3 = fizzbuzz(3)
+    "Fizz"
+
+    elsif int % 5 == 0
+    #fizz_5 = fizzbuzz(5)
+    "Buzz"
+
+
+end
+
+end
