4fce007d71fcdaeff9f1c5aa42482fc36609e3d4
Ashley Ryan <ashleyskyeryan@gmail.com>
2017-07-14 14:39:42 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 def fizzbuzz(user_number)
   user_number = gets.to_i
-  if user_number / 3 == 0 puts "Fizz"
-  elsif user_number / 5 == 0 puts "Buzz"
-  elsif user_number / 15 == 0 puts "FizzBuzz"
-  else puts nil
+  if user_number / 3 == % 0 puts "Fizz"
+  elsif user_number / 5 == % 0 puts "Buzz"
+  elsif user_number / 15 == % 0 puts "FizzBuzz"
+  else puts "nil"
 end
