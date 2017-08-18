6ec76ffacc36087556ffaeb47fe437ed8adc75d6
Arpan Kaul <kaul.arpan@gmail.com>
2017-08-18 21:26:09 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,11 @@
+def fizzbuzz(i)
+  if (i % 15) == 0
+    return "FizzBuzz"
+  elsif (i % 5) == 0
+    return "Buzz"
+  elsif (i % 3) == 0
+    return "Fizz"
+  else (i) == 0
+    return nil
+  end
+end
