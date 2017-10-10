aaa4e38539295a3c05baada8dbbbad80403d7c3f
abdillahi09 <abdillahi.muse@hotmail.com>
2017-10-10 06:25:13 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,16 @@
+# Defining a method named "fizzbuzz" in the next line of code:
+def fizzbuzz(int)
+
+  if int % 3 == 0 && int % 5 == 0
+   return "FizzBuzz"
+  end
+
+  if int % 5 == 0
+   return "Buzz"
+  end
+
+  if int % 3 == 0
+   return "Fizz"
+  end
+
+end
