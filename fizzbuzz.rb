ba82712a2132ecacee268512251e7e8fe4c5653a
bennybop <bennybop74@yahoo.com>
2017-10-15 13:44:13 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,19 @@
+def fizzbuzz(int)
+if int % 3 == 0 && int % 5 == 0
+  puts "FizzBuzz"
+   "FizzBuzz"
+
+elsif int % 3 == 0
+  print  "Fizz"
+   "Fizz"
+
+
+ elsif int % 5 == 0
+     print "Buzz"
+      "Buzz"
+
+
+
+
+end
+end
