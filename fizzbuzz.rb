65dc06fe9697bfefde3ed689de65ef0b70e3f882
Olivia Macedo <liom62313@gmail.com>
2018-08-22 03:48:35 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,20 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(value)
+  answer = " "
+  failure = 0
+  if(value % 3 == 0 && value % 5 != 0)
+    answer = "Fizz"
+    return answer
+  elsif(value % 5 == 0 && value % 3 != 0)
+    answer = "Buzz"
+    return answer
+  elsif(value % 3 == 0 && value % 5 == 0)
+    answer = "FizzBuzz"
+    return answer
+  else
+    failure = nil
+    return failure
+  end
+end
